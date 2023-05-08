9a9faa5935d5ce73a393fe343dcd6086ac7417b4
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-08 10:32:30 +0900

    fix: 불필요한 코드 삭제

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -3,31 +3,3 @@ export const API_URL = "http://192.168.10.9:38080";
 export const DASHBOARD_API = "/api/v1/tibco/dash/get/top";
 
 export const EMS_SERVER_API = "/api/v1/tibco/ems/get/queue";
-
-// const ReactGridExample: React.FC<Props> = () => {
-//   const [people, setPeople] = useState<Person[]>([]);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       try {
-//         const response = await axios.post("/api/v1/tibco/ems/get/queue", {
-//           // request data
-//         });
-//         setPeople(response.data);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     };
-//     fetchData();
-//   }, []);
-
-//   const rows = [
-//     headerRow,
-//     ...people.map<Row>((person, idx) => ({
-//       rowId: idx,
-//       cells: [
-//         { type: "text", text: person.name },
-//         { type: "text", text: person.surname },
-//       ],
-//     })),
-//   ];
