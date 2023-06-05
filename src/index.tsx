00413ee5b4eb77e68dd5386c3bebe0a8cbba2ffa
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-06-05 14:28:13 +0900

    fix: 파일 경로 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { render } from "react-dom";
-import ReactGridExample from "./constant/reactGrid";
+import ReactGridExample from "./constant/reactGridExample";
+import ReactGridTest from "./constant/reactGridTest";
 
 function App() {
-  return <ReactGridExample />;
+  return <ReactGridTest />;
 }
 
 render(<App />, document.getElementById("root"));
